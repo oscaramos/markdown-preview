46b00efaba9600dc0641844ea54ebe8b7c1cd84a
oscaramos <odrr.99@gmail.com>
2021-10-22 00:48:44 -0500

    Simplified welcome routing

--- src/pages/Welcome/Component.js
+++ src/pages/Welcome/Component.js
@@ -1,44 +1,16 @@
 import React from "react";
 
-import Container from "@material-ui/core/Container";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-
-import { FaReact as ReactIcon } from "react-icons/fa";
-
-import Meta from "components/Meta";
-
-import useStyles from "./styles";
-
 import { useHistory } from "react-router-dom";
+import { useStore } from "../../store";
 
 function Welcome() {
-  const matchSmallScreen = useMediaQuery("(max-width: 600px)");
-  const classes = useStyles({ isSmallScreen: matchSmallScreen });
+  const { state } = useStore();
 
   // Redirect to the first page
   const history = useHistory();
-  history.push("/document/0");
+  history.push(`/document/${state.documents[0].id}`);
 
-  return (
-    <>
-      <Meta title="Welcome" description="Welcome to React PWA" />
-      <Container maxWidth="sm" className={classes.root}>
-        <Box className={classes.wrapper}>
-          <Box className={classes.iconBox}>
-            <ReactIcon className={classes.icon} />
-          </Box>
-          <Typography
-            variant={matchSmallScreen ? "h4" : "h3"}
-            className={classes.title}
-          >
-            Redirecting to Markdown Pages
-          </Typography>
-        </Box>
-      </Container>
-    </>
-  );
+  return null;
 }
 
 export default Welcome;
